ff7b20f61b53e2df4af88b7103286a14b1d6dcec
satoufuyuki <satoufuyuki@proton.me>
2024-11-30 21:42:10 +0700

    chore: comment seeder

--- composeApp/src/desktopMain/kotlin/dev/pbt/casigma/modules/database/Seeder.kt
+++ composeApp/src/desktopMain/kotlin/dev/pbt/casigma/modules/database/Seeder.kt
@@ -7,39 +7,39 @@ import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    val db = Database()
-    val argon2 = Argon2()
-    transaction(db.conn) {
-        // Admin
-        User.insert {
-            it[username] = "admin"
-            it[password] = argon2.hash("admin")
-            it[role] = UserRole.Admin
-            it[name] = "Admin"
-        }
-
-        // Waiters
-        User.insert {
-            it[username] = "waiters"
-            it[password] = argon2.hash("waiters")
-            it[role] = UserRole.Waiters
-            it[name] = "Waiters"
-        }
-
-        // Cashiers
-        User.insert {
-            it[username] = "cashier"
-            it[password] = argon2.hash("cashier")
-            it[role] = UserRole.Cashier
-            it[name] = "Cashier"
-        }
-
-        // Chefs
-        User.insert {
-            it[username] = "chef"
-            it[password] = argon2.hash("chef")
-            it[role] = UserRole.Chef
-            it[name] = "Chef"
-        }
-    }
+//    val db = Database()
+//    val argon2 = Argon2()
+//    transaction(db.connect()) {
+//        // Admin
+//        User.insert {
+//            it[username] = "admin"
+//            it[password] = argon2.hash("admin")
+//            it[role] = UserRole.Admin
+//            it[name] = "Admin"
+//        }
+//
+//        // Waiters
+//        User.insert {
+//            it[username] = "waiters"
+//            it[password] = argon2.hash("waiters")
+//            it[role] = UserRole.Waiters
+//            it[name] = "Waiters"
+//        }
+//
+//        // Cashiers
+//        User.insert {
+//            it[username] = "cashier"
+//            it[password] = argon2.hash("cashier")
+//            it[role] = UserRole.Cashier
+//            it[name] = "Cashier"
+//        }
+//
+//        // Chefs
+//        User.insert {
+//            it[username] = "chef"
+//            it[password] = argon2.hash("chef")
+//            it[role] = UserRole.Chef
+//            it[name] = "Chef"
+//        }
+//    }
 }
