c60c53b884f8041e8764565d83346c22e6798715
satoufuyuki <satoufuyuki@proton.me>
2024-11-30 15:10:11 +0700

    fix(OrderUtils): opt-out null menus

--- composeApp/src/desktopMain/kotlin/dev/pbt/casigma/modules/utils/OrderUtils.kt
+++ composeApp/src/desktopMain/kotlin/dev/pbt/casigma/modules/utils/OrderUtils.kt
@@ -15,8 +15,8 @@ import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class OrderUtils(private val db: Database) {
-    fun fetchOrders(status: OrderStatus?): Map<Int, OrderObject> {
-        val ordersMap = mutableMapOf<Int, OrderObject>()
+    fun fetchOrder(orderId: Int): OrderObject? {
+        var result: OrderObject? = null
 
         transaction (db.conn) {
             val query = Join(
@@ -24,6 +24,60 @@ class OrderUtils(private val db: Database) {
                 onColumn = Order.id, otherColumn = OrderItem.orderId,
                 joinType = JoinType.INNER,
             ).join(Menu, JoinType.INNER, OrderItem.menuId, Menu.id)
+
+            val res = query.selectAll()
+                .where { Order.id eq orderId }
+                .toList()
+
+            if (!res.any()) {
+                return@transaction
+            }
+
+            val orderItems = arrayListOf<OrderItemObject>()
+            res.forEach {
+                if (it[Menu.id] != null) {
+                    val menuItem = MenuObject(
+                        id = it[Menu.id].value,
+                        name = it[Menu.name],
+                        price = it[Menu.price],
+                        category = it[Menu.category],
+                        image = it[Menu.image],
+                        createdAt = it[Menu.createdAt]
+                    )
+
+                    orderItems.add(
+                        OrderItemObject(
+                            id = it[OrderItem.id].value,
+                            quantity = it[OrderItem.quantity],
+                            menu = menuItem
+                        )
+                    )
+                }
+            }
+
+            result = OrderObject(
+                id = orderId,
+                createdAt = res.first()[Order.createdAt],
+                additionalNotes = res.first()[Order.additionalNotes],
+                tableNo = res.first()[Order.tableNo],
+                items = orderItems,
+                customerName = res.first()[Order.customerName],
+                status = res.first()[Order.status],
+            )
+        }
+
+        return result
+    }
+
+    fun fetchOrders(status: OrderStatus?): Map<Int, OrderObject> {
+        val ordersMap = mutableMapOf<Int, OrderObject>()
+
+        transaction (db.conn) {
+            val query = Join(
+                Order, OrderItem,
+                onColumn = Order.id, otherColumn = OrderItem.orderId,
+                joinType = JoinType.LEFT,
+            ).join(Menu, JoinType.LEFT, OrderItem.menuId, Menu.id)
 
             var res: List<ResultRow> = arrayListOf()
             if (status != null) {
@@ -37,30 +91,42 @@ class OrderUtils(private val db: Database) {
 
             res.forEach {
                 val orderId = it[Order.id].value
-                val menuItem = MenuObject(
-                    id = it[Menu.id].value,
-                    name = it[Menu.name],
-                    price = it[Menu.price],
-                    category = it[Menu.category],
-                    image = it[Menu.image],
-                    createdAt = it[Menu.createdAt]
-                )
+                if (it[Menu.id] != null) {
+                    val menuItem = MenuObject(
+                        id = it[Menu.id].value,
+                        name = it[Menu.name],
+                        price = it[Menu.price],
+                        category = it[Menu.category],
+                        image = it[Menu.image],
+                        createdAt = it[Menu.createdAt]
+                    )
 
-                val orderItem = OrderItemObject(
-                    id = it[OrderItem.id].value,
-                    quantity = it[OrderItem.quantity],
-                    menu = menuItem
-                )
+                    val orderItem = OrderItemObject(
+                        id = it[OrderItem.id].value,
+                        quantity = it[OrderItem.quantity],
+                        menu = menuItem
+                    )
 
-                if (ordersMap.containsKey(orderId)) {
-                    ordersMap[orderId]?.items?.add(orderItem)
+                    if (ordersMap.containsKey(orderId)) {
+                        ordersMap[orderId]?.items = listOf(orderItem) + ordersMap[orderId]?.items!!
+                    } else {
+                        ordersMap[orderId] = OrderObject(
+                            id = orderId,
+                            createdAt = it[Order.createdAt],
+                            additionalNotes = it[Order.additionalNotes],
+                            tableNo = it[Order.tableNo],
+                            items = arrayListOf(orderItem),
+                            customerName = it[Order.customerName],
+                            status = it[Order.status],
+                        )
+                    }
                 } else {
                     ordersMap[orderId] = OrderObject(
                         id = orderId,
                         createdAt = it[Order.createdAt],
                         additionalNotes = it[Order.additionalNotes],
                         tableNo = it[Order.tableNo],
-                        items = arrayListOf(orderItem),
+                        items = arrayListOf(),
                         customerName = it[Order.customerName],
                         status = it[Order.status],
                     )
