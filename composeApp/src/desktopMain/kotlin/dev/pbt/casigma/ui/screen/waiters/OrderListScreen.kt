1cfc22036a1cb8225ed650fe0884d0c2811336f2
satoufuyuki <satoufuyuki@proton.me>
2024-11-30 15:13:39 +0700

    feat(screens/OrderListScreen): implement buttons and more

--- composeApp/src/desktopMain/kotlin/dev/pbt/casigma/ui/screen/waiters/OrderListScreen.kt
+++ composeApp/src/desktopMain/kotlin/dev/pbt/casigma/ui/screen/waiters/OrderListScreen.kt
@@ -6,42 +6,73 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import dev.pbt.casigma.ui.components.DropdownTable
+import androidx.navigation.NavHostController
+import dev.pbt.casigma.AppScreen
+import dev.pbt.casigma.GlobalContext
+import dev.pbt.casigma.modules.database.models.OrderStatus
+import dev.pbt.casigma.modules.providers.DialogProvider
+import dev.pbt.casigma.modules.utils.AlertUtils
+import dev.pbt.casigma.modules.utils.TextUtils
+import dev.pbt.casigma.modules.viewmodel.TableViewModel
 import dev.pbt.casigma.ui.components.OrderCard
 import dev.pbt.casigma.ui.components.OrderDetail
 import dev.pbt.casigma.ui.screen.ScreenBase
 import dev.pbt.casigma.ui.theme.neutral
 import dev.pbt.casigma.ui.theme.white
+import org.koin.compose.koinInject
+import org.koin.core.qualifier.named
 
 class OrderListScreen(override val route: String): ScreenBase(route) {
     @Composable
     @OptIn(ExperimentalLayoutApi::class)
     override fun render() {
-        val state = rememberScrollState()
-        val currentActiveOrder = remember {
-            mutableStateOf(0)
+        val dialogProvider: DialogProvider = koinInject()
+        val globalManagedOrderId: MutableState<Int?> = koinInject(qualifier = named(GlobalContext.ManagedOrderId))
+        val navHostController: NavHostController = koinInject()
+        val tableScrollState = rememberScrollState()
+        val tableViewModel = koinInject<TableViewModel>()
+        val tables by tableViewModel.tableState.collectAsState()
+        val currentActiveOrder = remember { mutableStateOf<Int?>(null) }
+        val currentActiveCategory = remember { mutableStateOf(OrderStatus.Pending) }
+        val orderCategory = listOf(OrderStatus.Pending, OrderStatus.Completed)
+
+        // Initial fetch
+        if (tableViewModel.tableState.value.isEmpty()) {
+            tableViewModel.fetchOrder(currentActiveCategory.value)
         }
 
-        val isDropDownExpanded = remember {
-            mutableStateOf(false)
+        val manageOrder = { orderId: Int? ->
+            if (orderId != null) {
+                globalManagedOrderId.value = orderId
+                navHostController.navigate(AppScreen.WaitersRecordOrder.name)
+            }
         }
 
-        val currentActiveCategory = remember {
-            mutableStateOf(0)
+        val deleteOrder = { orderId: Int? ->
+            if (orderId != null) {
+                dialogProvider.setAlertComponent {
+                    AlertUtils.buildPromptDecision(
+                        "Are you sure you want to remove this order?",
+                        onConfirm = {
+                            try {
+                                tableViewModel.removeOrder(orderId)
+                            } catch (e: Exception) {
+                                dialogProvider.setAlertComponent {
+                                    AlertUtils.buildError("Failed to remove order: ${e.message}")
+                                }.show()
+                            }
+                        }
+                    )
+                }.show()
+            }
         }
-
-        val mockData = listOf("Table 1", "Table 2", "Table 3")
-        val orderCategory = listOf("All", "Pending", "Confirmed")
-
         // Container for the view
         Row(
             modifier = Modifier.fillMaxWidth().background(neutral),
@@ -65,20 +96,19 @@ class OrderListScreen(override val route: String): ScreenBase(route) {
                         fontWeight = FontWeight.Medium,
                         fontSize = 40.sp
                     )
-//                    DropdownTable(mockData)
                 }
                 Row (
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(24.dp, Alignment.CenterHorizontally),
                 ) {
-                    orderCategory.forEachIndexed { index, category ->
-                        TextButton(onClick = { currentActiveCategory.value = index }) {
+                    orderCategory.forEach { category ->
+                        TextButton(onClick = { currentActiveCategory.value = category }) {
                             Text(
-                                category,
+                                TextUtils.toTitleCase(category.status),
                                 style = MaterialTheme.typography.bodyMedium,
                                 fontWeight = FontWeight.Medium,
                                 fontSize = 25.sp,
-                                color = if (currentActiveCategory.value == index) Color.Black else Color.Gray,
+                                color = if (currentActiveCategory.value == category) Color.Black else Color.Gray,
                             )
                         }
                     }
@@ -87,18 +117,27 @@ class OrderListScreen(override val route: String): ScreenBase(route) {
                 FlowColumn (
                     horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.Start),
                     verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top),
-                    modifier = Modifier.fillMaxWidth().verticalScroll(state),
+                    modifier = Modifier.fillMaxWidth().verticalScroll(tableScrollState),
                     overflow = FlowColumnOverflow.Clip
                 ) {
-                    for (i in 0..19) {
-                        OrderCard(onClick = { currentActiveOrder.value = i }, enabled = currentActiveOrder.value == i)
+                    tables.forEachIndexed { index, it ->
+                        OrderCard(
+                            tableNo = it.tableNo,
+                            time = it.createdAt,
+                            enabled = currentActiveOrder.value == index,
+                            onClick = { currentActiveOrder.value = index }
+                        )
                     }
                 }
             }
             Column (
                 modifier = Modifier.fillMaxWidth().fillMaxHeight().background(white)
             ) {
-                OrderDetail()
+                OrderDetail(
+                    currentActiveOrder.value?.let { tables.getOrNull(it) },
+                    buttonAction = { manageOrder(currentActiveOrder.value?.let { tables.getOrNull(it)?.id }) },
+                    deleteAction = { deleteOrder(currentActiveOrder.value?.let { tables.getOrNull(it)?.id }) }
+                )
             }
         }
     }
