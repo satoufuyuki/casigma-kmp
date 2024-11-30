ddd4d926ef480b24feffdfbbc8d04ff283388357
satoufuyuki <satoufuyuki@proton.me>
2024-11-30 15:13:52 +0700

    feat(screens/RecordOrderScreen): implement buttons and more

--- composeApp/src/desktopMain/kotlin/dev/pbt/casigma/ui/screen/waiters/RecordOrderScreen.kt
+++ composeApp/src/desktopMain/kotlin/dev/pbt/casigma/ui/screen/waiters/RecordOrderScreen.kt
@@ -6,17 +6,15 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.pbt.casigma.GlobalContext
 import dev.pbt.casigma.modules.database.Database
 import dev.pbt.casigma.modules.database.models.Menu
 import dev.pbt.casigma.modules.database.models.MenuCategory
@@ -25,7 +23,12 @@ import dev.pbt.casigma.modules.database.models.Order
 import dev.pbt.casigma.modules.database.models.OrderItemObject
 import dev.pbt.casigma.modules.database.models.OrderObject
 import dev.pbt.casigma.modules.database.models.OrderStatus
+import dev.pbt.casigma.modules.providers.DialogProvider
+import dev.pbt.casigma.modules.utils.AlertUtils
 import dev.pbt.casigma.modules.utils.OrderUtils
+import dev.pbt.casigma.modules.utils.TextUtils
+import dev.pbt.casigma.modules.viewmodel.MenuViewModel
+import dev.pbt.casigma.modules.viewmodel.TableViewModel
 import dev.pbt.casigma.ui.components.DropdownTable
 import dev.pbt.casigma.ui.components.MenuCard
 import dev.pbt.casigma.ui.components.OrderDetail
@@ -34,100 +37,68 @@ import dev.pbt.casigma.ui.theme.neutral
 import dev.pbt.casigma.ui.theme.white
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.koin.compose.koinInject
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.qualifier.named
+import java.util.*
 
-class RecordOrderScreen(override val route: String, private val db: Database, private val orderUtils: OrderUtils): ScreenBase(route) {
+class RecordOrderScreen(override val route: String): ScreenBase(route) {
     @OptIn(ExperimentalLayoutApi::class)
     @Composable
-    override fun render() {
-        val menus = remember {
-            mutableStateListOf<MenuObject>()
-        }
-
-        val availableTables = remember {
-            mutableStateListOf<Int>()
-        }
-
-        val currentOrder: MutableState<OrderObject?> = remember {
-            mutableStateOf(null)
-        }
-
-        val currentOrderItems = remember {
-            mutableStateListOf<OrderItemObject>()
-        }
-
-        val currentActiveTable = remember {
-            mutableStateOf(-1)
-        }
-
-        transaction(db.conn) {
-            if (menus.isEmpty()) {
-                Menu.selectAll().forEach {
-                    menus.add(
-                        MenuObject(
-                            id = it[Menu.id].value,
-                            name = it[Menu.name],
-                            price = it[Menu.price],
-                            category = it[Menu.category],
-                            createdAt = it[Menu.createdAt],
-                            image = it[Menu.image]
-                        )
-                    )
-                }
-            }
-
-            if (availableTables.isEmpty()) {
-                Order.select(Order.tableNo).forEach {
-                    availableTables.add(it[Order.tableNo])
-                }
-            }
-        }
-
+    override fun render(
+    ) {
+        val globalManagedOrderId: MutableState<Int?> = koinInject(qualifier = named(GlobalContext.ManagedOrderId))
+        val dialogProvider: DialogProvider = koinInject()
+        val menuViewModel: MenuViewModel = koinViewModel()
+        val tableViewModel: TableViewModel = koinViewModel()
+        val menus by menuViewModel.menus.collectAsState()
         val menuScrollState = rememberScrollState()
-        val currentActiveCategory = remember {
-            mutableStateOf(0)
-        }
-
+        val tables by tableViewModel.tableState.collectAsState()
         val menuCategory = listOf(MenuCategory.Food, MenuCategory.Beverages, MenuCategory.Dessert)
-
-        fun handleQuantityButton(action: String, item: MenuObject) {
-            if (currentOrder.value == null) {
-                return
-            }
-
-            var existingItem = currentOrder.value?.items?.find { it.menu.id == item.id }
-            when (action) {
-                "add" -> {
-                   if (existingItem != null) {
-                       val index = currentOrderItems.indexOf(existingItem)
-                       currentOrderItems[index] = existingItem.copy(quantity = existingItem.quantity + 1)
-                   } else {
-                       currentOrder.value?.items?.add(
-                           OrderItemObject(
-                               id = 0,
-                               quantity = 1,
-                               menu = item
-                           )
-                       )
-                   }
-                }
-                "remove" -> {
-                    if (existingItem != null) {
-                        if (existingItem.quantity > 1) {
-                            val index = currentOrderItems.indexOf(existingItem)
-                            currentOrderItems[index] = existingItem.copy(quantity = existingItem.quantity - 1)
-                        } else {
-                            currentOrderItems.remove(existingItem)
-                        }
-                    }
+        val currentActiveCategory = remember { mutableStateOf(0) }
+        val currentActiveOrderId = remember { mutableStateOf<Int?>(null) }
+        val menuItems by remember {
+            derivedStateOf {
+                menus.filter { it.category.ordinal == currentActiveCategory.value }.map { menu ->
+                    val currentOrder = tables.find { it.id == currentActiveOrderId.value }
+                    val quantity = currentOrder?.items?.find { it.menu.id == menu.id }?.quantity ?: 0
+                    menu to quantity
                 }
             }
         }
 
-        fun handleTableSelection(tableIndex: Int) {
-            currentOrder.value = orderUtils.fetchOrders(OrderStatus.Pending)[tableIndex]
-            currentActiveTable.value = tableIndex
-            currentOrderItems.clear()
-            currentOrderItems.addAll(currentOrder.value?.items ?: listOf())
+        val saveOrder = {
+            currentActiveOrderId.value?.let {
+                try {
+                    tableViewModel.saveOrder(it)
+                    dialogProvider.setAlertComponent {
+                        AlertUtils.buildSuccess("Order saved successfully!")
+                    }.show()
+                } catch (e: Exception) {
+                    dialogProvider.setAlertComponent {
+                        AlertUtils.buildError("Failed to save order: ${e.message}")
+                    }.show()
+                }
+            }
+        }
+
+        val handleTableSelection = { tableIndex: Int ->
+            tables.getOrNull(tableIndex)?.let {
+                currentActiveOrderId.value = it.id
+            }
+        }
+
+        if (tables.isEmpty()) {
+            tableViewModel.fetchOrder(OrderStatus.Pending)
+        } else {
+            // Check previous global managed order
+            globalManagedOrderId.value?.let { orderId ->
+                globalManagedOrderId.value = null // clear at first boot
+                val index = tables.indexOf(tables.find { it.id == orderId })
+                if (index != -1) {
+                    handleTableSelection(index)
+                }
+            }
         }
 
         // Container for the view
@@ -154,8 +125,8 @@ class RecordOrderScreen(override val route: String, private val db: Database, pr
                         fontSize = 40.sp
                     )
                     DropdownTable(
-                        tables = availableTables.map { "$it" },
-                        currentTableIndex = currentActiveTable.value,
+                        tables = tables.map { it.tableNo.toString() },
+                        currentTableIndex = tables.indexOf(tables.find { it.id == currentActiveOrderId.value }),
                         onTableSelected = {
                             handleTableSelection(it)
                         }
@@ -165,44 +136,60 @@ class RecordOrderScreen(override val route: String, private val db: Database, pr
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(24.dp, Alignment.CenterHorizontally),
                 ) {
-                    menuCategory.forEachIndexed { index, category ->
-                        TextButton(onClick = { currentActiveCategory.value = index }) {
+                    menuCategory.forEachIndexed { _, category ->
+                        TextButton(onClick = { currentActiveCategory.value = category.ordinal }) {
                             Text(
-                                category.category,
+                                TextUtils.toTitleCase(category.category),
                                 style = MaterialTheme.typography.bodyMedium,
                                 fontWeight = FontWeight.Medium,
                                 fontSize = 25.sp,
-                                color = if (currentActiveCategory.value == index) Color.Black else Color.Gray,
+                                color = if (currentActiveCategory.value == category.ordinal) Color.Black else Color.Gray,
                             )
                         }
                     }
                 }
                 // Menu Card
-                FlowRow (
-                    horizontalArrangement = Arrangement.spacedBy(24.dp, Alignment.Start),
-                    verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.Top),
-                    modifier = Modifier.fillMaxWidth().verticalScroll(menuScrollState),
-                    overflow = FlowRowOverflow.Visible
-                ) {
-                    menus.forEach {
-                        if (it.category == menuCategory[currentActiveCategory.value]) {
+                if (currentActiveOrderId.value != null) { // Don't show menu if no table is selected
+                    FlowRow (
+                        horizontalArrangement = Arrangement.spacedBy(24.dp, Alignment.Start),
+                        verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.Top),
+                        modifier = Modifier.fillMaxWidth().verticalScroll(menuScrollState),
+                        overflow = FlowRowOverflow.Visible
+                    ) {
+                        menuItems.forEach { (it, quantity) ->
                             MenuCard(
                                 name = it.name,
                                 price = it.price,
                                 image = it.image,
-                                onAdd = { handleQuantityButton("add", it) },
-                                onRemove = { handleQuantityButton("remove", it) },
-                                quantity = currentOrderItems.find { item -> item.menu.id == it.id }?.quantity ?: 0
+                                onAdd = { tableViewModel.incrementOrderItemQuantity(currentActiveOrderId.value!!, it) },
+                                onRemove = { tableViewModel.decrementOrderItemQuantity(currentActiveOrderId.value!!, it.id) },
+                                quantity = quantity
                             )
                         }
+                    }
+                } else {
+                    Box(
+                        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            "Please select a table to start recording!",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Medium,
+                            fontSize = 30.sp
+                        )
                     }
                 }
             }
             Column (
                 modifier = Modifier.fillMaxWidth().fillMaxHeight().background(white)
             ) {
-                OrderDetail()
+                OrderDetail(
+                    tables.find { it.id == currentActiveOrderId.value },
+                    buttonAction = { saveOrder()}
+                )
             }
         }
     }
 }
+
